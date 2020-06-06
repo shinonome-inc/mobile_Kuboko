cff208eb7df03920b879cbd3bb707f854612ccef
Kuboko <kuboko1201@gmail.com>
2020-06-06 13:46:50 +0900

    Fixed　code
    課題2を修正しました
    switch文を消して、rawValueからenumの値を取得するように変更しました

--- Kadai02Janken/janken.playground/Contents.swift
+++ Kadai02Janken/janken.playground/Contents.swift
@@ -1,39 +1,26 @@
 //ジャンケンの手
 enum Hand : Int {
-    case rock = 0
-    case scissors = 1
-    case paper = 2
+    case rock       //0(デフォルト値)
+    case scissors   //1
+    case paper      //2
 }
 
 //ジャンケンの結果
-enum Result : String {
-    case draw = "you draw"
-    case lose = "you loss"
-    case win = "you win" 
+enum Result : Int {
+    case draw       //0(デフォルト値)
+    case lose       //1
+    case win        //2
 }
 
 //ジャンケンメソッド
 func janken(you: Hand) {
-    let cp = Int.random(in: 0 ... 2)         //cpHand   0rock 1:scissors 2:paper
-    let judge = (you.rawValue - cp + 3) % 3  //judge    0:draw 1:loss 2:win
-    
-    switch cp {
-    case 0:
-        print("cp: \(Hand.rock).", terminator:" ")
-    case 1:
-        print("cp: \(Hand.scissors).", terminator:" ")
-    default:
-        print("cp: \(Hand.paper).", terminator:" ")
+    guard let cp = Hand(rawValue: Int.random(in: 0...2)) else { //cp   0:rock 1:scissors 2:paper
+       fatalError("ERROR HAND TYPE")
     }
-    
-    switch judge {
-    case 0:
-        print(Result.draw.rawValue)
-    case 1:
-        print(Result.lose.rawValue)
-    default:
-        print(Result.win.rawValue)
+    guard let result = Result(rawValue: (you.rawValue - cp.rawValue + 3) % 3) else {   //judge    0:draw 1:loss 2:win
+        fatalError("ERROR RESULT TYPE")
     }
+    print("you: \(you).  cp: \(cp).  you \(result)")
 }
 
 janken(you: .rock)
