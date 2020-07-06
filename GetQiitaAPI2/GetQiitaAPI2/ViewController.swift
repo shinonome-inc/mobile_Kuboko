0d1dda9a5546236e1bef61d271593bff3adc58be
Kuboko <kuboko1201@gmail.com>
2020-07-06 11:47:05 +0900

    変更

---
+++ GetQiitaAPI2/GetQiitaAPI2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetQiitaAPI2
+//
+//  Created by Koki Kubota on 2020/07/05.
+//  Copyright © 2020 Koki Kubota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
