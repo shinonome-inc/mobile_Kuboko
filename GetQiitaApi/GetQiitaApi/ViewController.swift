65323a59e8b49c8e1eed82c903d7bb25e0f897d8
Kuboko <kuboko1201@gmail.com>
2020-07-06 19:19:15 +0900

    push GetQiitaApi

---
+++ GetQiitaApi/GetQiitaApi/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GetQiitaApi
+//
+//  Created by Koki Kubota on 2020/07/06.
+//  Copyright © 2020 Koki Kubota. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GetQiitaApi/Pods/Target Support Files/Pods-GetQiitaApi-GetQiitaApiUITests/Pods-GetQiitaApi-GetQiitaApiUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GetQiitaApi_GetQiitaApiUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GetQiitaApi_GetQiitaApiUITests
+@end
