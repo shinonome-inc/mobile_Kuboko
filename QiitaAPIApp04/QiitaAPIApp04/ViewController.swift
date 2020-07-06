63ab17a7dbff4c57bca111ae061a2f678e05b99d
Kuboko <kuboko1201@gmail.com>
2020-07-06 12:29:04 +0900

    チェックお願いします
    .gitignore がうまくいってない気がします。
    また訂正します

---
+++ QiitaAPIApp04/QiitaAPIApp04/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  QiitaAPIApp04
+//
+//  Created by Koki Kubota on 2020/07/06.
+//  Copyright © 2020 Koki Kubota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
