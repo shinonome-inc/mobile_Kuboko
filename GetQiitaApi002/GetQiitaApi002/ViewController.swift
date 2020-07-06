cc20edaa1cbe8b860e18389eca13666cb6e6e35d
Kuboko <kuboko1201@gmail.com>
2020-07-06 21:21:55 +0900

    ignore test001

---
+++ GetQiitaApi002/GetQiitaApi002/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetQiitaApi002
+//
+//  Created by Koki Kubota on 2020/07/06.
+//  Copyright © 2020 Koki Kubota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
