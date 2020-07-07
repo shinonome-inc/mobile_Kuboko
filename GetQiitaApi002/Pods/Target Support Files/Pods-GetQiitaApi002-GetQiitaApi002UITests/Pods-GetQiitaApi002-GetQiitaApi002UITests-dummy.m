9b80dc8b836dc249dfa67874ce4500e3eccaefd0
Kuboko <kuboko1201@gmail.com>
2020-07-07 11:46:31 +0900

    Kadai04Janken App 修正済

---
+++ GetQiitaApi002/Pods/Target Support Files/Pods-GetQiitaApi002-GetQiitaApi002UITests/Pods-GetQiitaApi002-GetQiitaApi002UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GetQiitaApi002_GetQiitaApi002UITests : NSObject
+@end
+@implementation PodsDummy_Pods_GetQiitaApi002_GetQiitaApi002UITests
+@end
---
+++ GetQiitaApi002/Pods/Target Support Files/Pods-GetQiitaApi002/Pods-GetQiitaApi002-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GetQiitaApi002 : NSObject
+@end
+@implementation PodsDummy_Pods_GetQiitaApi002
+@end
